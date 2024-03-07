a167f892dbad231aa3a27bc6f95586229c363274
Anderson Balbino <113153237+AndBalbino@users.noreply.github.com>
2024-03-07 07:26:02 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,34 +7,6 @@ document.addEventListener('DOMContentLoaded', function(){
     // Seção das imagens
     const buttons = document.querySelectorAll(".content__order__item");
     const image = document.querySelector("#image-content");
-
-
-    // //Seção do timer
-    // const contaOTempo = setInterval(function (){
-    //     const agora = new Date();
-        
-        
-    //     const timeStampAtual = agora.getTime();
-    
-    //     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
-    
-    //     const diaEmMs = 1000 * 60 * 60 * 24;
-    //     const horaEmMs = 1000 * 60 * 60;
-    //     const minutoEmMs = 1000 * 60;
-        
-    
-    //     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    //     const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
-    //     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
-    //     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
-    
-    //     document.getElementById('contador').innerHTML = `${diasAteOEvento} Dias ${horasAteOEvento} Hrs ${minutosAteOEvento} Min ${segundosAteOEvento} Seg`
-    
-    //     if(distanciaAteOEvento < 0){
-    //         clearInterval(contaOTempo);
-    //         document.querySelector('.hero__text').textContent = `Promoção expirou`;
-    //     } 
-    // }, 1000);
      const contaOTempo = setInterval(function (){
         const agora = new Date();
         const timeStampAtual = agora.getTime();
