c4a31d8b093fa76f10ccd39d69db15985b40d177
Anderson Balbino <113153237+AndBalbino@users.noreply.github.com>
2024-03-06 23:11:28 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,34 @@ document.addEventListener('DOMContentLoaded', function(){
     const image = document.querySelector("#image-content");
 
 
-    //Seção do timer
-    const contaOTempo = setInterval(function (){
+    // //Seção do timer
+    // const contaOTempo = setInterval(function (){
+    //     const agora = new Date();
+        
+        
+    //     const timeStampAtual = agora.getTime();
+    
+    //     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    
+    //     const diaEmMs = 1000 * 60 * 60 * 24;
+    //     const horaEmMs = 1000 * 60 * 60;
+    //     const minutoEmMs = 1000 * 60;
+        
+    
+    //     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    //     const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    //     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    //     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+    
+    //     document.getElementById('contador').innerHTML = `${diasAteOEvento} Dias ${horasAteOEvento} Hrs ${minutosAteOEvento} Min ${segundosAteOEvento} Seg`
+    
+    //     if(distanciaAteOEvento < 0){
+    //         clearInterval(contaOTempo);
+    //         document.querySelector('.hero__text').textContent = `Promoção expirou`;
+    //     } 
+    // }, 1000);
+     const contaOTempo = setInterval(function (){
         const agora = new Date();
-        
-        
         const timeStampAtual = agora.getTime();
     
         const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
@@ -22,20 +45,21 @@ document.addEventListener('DOMContentLoaded', function(){
         const horaEmMs = 1000 * 60 * 60;
         const minutoEmMs = 1000 * 60;
         
-    
-        const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-        const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
-        const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
-        const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
-    
-        document.getElementById('contador').innerHTML = `${diasAteOEvento} Dias ${horasAteOEvento} Hrs ${minutosAteOEvento} Min ${segundosAteOEvento} Seg`
-    
+        // Se o tempo já tiver passado
         if(distanciaAteOEvento < 0){
             clearInterval(contaOTempo);
-            document.querySelector('.hero__text').textContent = `Promoção expirou`;
-        } 
+            document.getElementById('contador').innerHTML = `Promoção expirou`;
+        } else {
+            // Se ainda estiver no tempo da promoção
+            const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+            const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+            const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+            const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+        
+            document.getElementById('contador').innerHTML = `${diasAteOEvento} Dias ${horasAteOEvento} Hrs ${minutosAteOEvento} Min ${segundosAteOEvento} Seg`;
+        }
     }, 1000);
-    
+
     //Alterando as imagens
     buttons.forEach((btn) => {
         btn.addEventListener("click", (e) => {
